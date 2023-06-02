437cd6b38c7504f2fe95827a338ca70b871d9c42
mizu <uwugoth266@gmail.com>
2023-06-02 20:20:21 +0800

    refactor: subscription plan to prisma

--- back-end/src/controllers/plan.js
+++ back-end/src/controllers/plan.js
@@ -1,9 +1,13 @@
+const { prisma } = require("../config/client")
+
 const getAllSubscriptionPlans = async (req, res) => {
   try {
-    const subscription_plan = await pool.query(
-      "select * from get_subscription_plan()"
-    )
-    return res.send(subscription_plan.rows)
+    const plan = await prisma.subscription_plan.findMany({
+      orderBy: {
+        plan_price: "asc",
+      },
+    })
+    return res.send(plan)
   } catch (err) {
     console.error(err)
     return res.status(500).send({ message: "Error executing query" })
